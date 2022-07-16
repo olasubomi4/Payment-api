07980087e5c525b6d9cf53643e8b56760d6f3093
Odekunle Olasubomi <40355148+olasubomi4@users.noreply.github.com>
2022-07-16 01:14:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const indexrouters = require("./routers/indexrouters");
+const port = process.env.PORT || 80;
 
 app.use(express.json());
 app.use(indexrouters);
 
-app.listen(process.env.PORT , () => {
+app.listen(port , () => {
 
     console.log("server started on port 5002");
 });
